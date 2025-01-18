c8fbc1093b6064a66bc63a11d5718a49a242d794
Malik Zouari <malek.zouari@iit.ens.tn>
2025-01-18 14:16:35 +0100

    Add requirements.txt and .gitignore & Tidy up README.md and Python script

--- README.md
+++ README.md
@@ -1,51 +1,40 @@
 # Bunny CDN "DRM" Video Downloader
 
-This is a simple Python class that I wrote to download Bunny CDN's "[DRM](https://bunny.net/stream/media-cage-video-content-protection/)" videos using [yt-dlp](https://github.com/yt-dlp/yt-dlp).
+A Python class for downloading Bunny
+CDN's "[DRM](https://bunny.net/stream/media-cage-video-content-protection/)" videos
+using [yt-dlp](https://github.com/yt-dlp/yt-dlp).
 
 ## Requirements
 
-You only need to install yt-dlp and then you may integrate it within your script and modify it according to your needs:
+You'll need to install `yt-dlp` and `requests` modules in your Python environment.
 
 ```bash
-pip install yt-dlp
+pip install -r requirements.txt
 ```
 
-or
-
-```bash
-python3 -m pip yt-dlp
-```
-> It is also better to have [FFmpeg](https://ffmpeg.org) installed on your system.
+> It is also better to have [FFmpeg](https://ffmpeg.org) installed on your system and its executable added to the PATH
+> environment variable.
 
 ## Usage
 
-If you're willing to use the script as is, simply paste the the iframe embed page URL at the bottom where indicated between the quotes as well as the webpage referer and run the script:
+To try the script for testing and demonstration, simply paste the iframe embed page URL at the bottom, where indicated
+between the quotes, as well as the webpage referer, and run the script.
 
 ```bash
 python3 b-cdn-drm-vod-dl.py
 ```
-> Embed link structure: [`https://iframe.mediadelivery.net/embed/{video_library_id}/{video_id}`](https://docs.bunny.net/docs/stream-embedding-videos)
 
-___
-
-Alternatively, you may want to run this command once:
-
-```bash
-chmod +x b-cdn-drm-vod-dl.py
-```
-
-after that you can run it this way:
-
-```bash
-./b-cdn-drm-vod-dl.py
-```
+> Embed link structure: [
+`https://iframe.mediadelivery.net/embed/{video_library_id}/{video_id}`](https://docs.bunny.net/docs/stream-embedding-videos)
 
 ## Expected Result
 
 By default:
 
-* the highest resolution video is to be downloaded. You can change this behavior in the `main_playlist` function located under the `prepare_dl` method.
-* The video will be downloaded in the `~/Videos/Bunny CDN/` directory. This configuration can be changed by providing the `path` argument when instantiating a new `BunnyVideoDRM` object.
+* the highest resolution video is to be downloaded. You can change this behavior in the `main_playlist` function located
+  under the `prepare_dl` method.
+* The video will be downloaded in the `~/Videos/Bunny CDN/` directory. This configuration can be changed by providing
+  the `path` argument when instantiating a new `BunnyVideoDRM` object.
 * The video file name will be extracted from the embed page. This can be overridden by providing the `name` argument .
 
 > Please note that the video format will be always `mp4`.
@@ -54,6 +43,11 @@ By default:
 
 The idea is all about simulating what's happening in a browser.
 
-The program runs a sequence of requests tied to a [session](https://requests.readthedocs.io/en/latest/user/advanced/#session-objects) first of which is the embed page request from which information are extracted such as the video name.
+The program runs a sequence of requests tied to
+a [session](https://requests.readthedocs.io/en/latest/user/advanced/#session-objects) object (for cookie persistence and
+connection pooling) first of which is the embed page request, from which information are extracted such as the video
+name.
 
-After that, the download link (that is the HLS/M3U8) is ready to be fed to yt-dlp to download the video segments, decrypt them (since Bunny CDN's "DRM" videos are encrypted with the AES-128 algorithm) and merge them into one playable video.
+After that, the download link (an HLS/M3U8 URL) is ready to be fed to `yt-dlp` to download the video segments, decrypt
+them (as Bunny CDN's "DRM" videos are encrypted with the AES-128 algorithm), and merge them into a single playable video
+file.
--- b-cdn-drm-vod-dl.py
+++ b-cdn-drm-vod-dl.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import re
 import sys
 from hashlib import md5
@@ -14,177 +12,177 @@ import yt_dlp
 class BunnyVideoDRM:
     # user agent and platform related headers
     user_agent = {
-        'sec-ch-ua':
-            '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-        'sec-ch-ua-mobile':
-            '?0',
-        'sec-ch-ua-platform':
-            '"Linux"',
-        'user-agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+        "sec-ch-ua": '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Linux"',
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
     }
-    session = requests.session()
+    session = requests.Session()
     session.headers.update(user_agent)
 
-    def __init__(self,
-                 referer='https://127.0.0.1/',
-                 embed_url='',
-                 name='',
-                 path=''):
+    def __init__(self, referer="https://127.0.0.1/", embed_url="", name="", path=""):
         self.referer = referer if referer else sys.exit(1)
         self.embed_url = embed_url if embed_url else sys.exit(1)
-        self.guid = urlparse(embed_url).path.split('/')[-1]
+        self.guid = urlparse(embed_url).path.split("/")[-1]
         self.headers = {
-            'embed': {
-                'authority': 'iframe.mediadelivery.net',
-                'accept': '*/*',
-                'accept-language': 'en-US,en;q=0.9',
-                'cache-control': 'no-cache',
-                'pragma': 'no-cache',
-                'referer': referer,
-                'sec-fetch-dest': 'iframe',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-site': 'cross-site',
-                'upgrade-insecure-requests': '1',
+            "embed": {
+                "authority": "iframe.mediadelivery.net",
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "no-cache",
+                "pragma": "no-cache",
+                "referer": referer,
+                "sec-fetch-dest": "iframe",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "cross-site",
+                "upgrade-insecure-requests": "1",
             },
-            'ping|activate': {
-                'accept': '*/*',
-                'accept-language': 'en-US,en;q=0.9',
-                'cache-control': 'no-cache',
-                'origin': 'https://iframe.mediadelivery.net',
-                'pragma': 'no-cache',
-                'referer': 'https://iframe.mediadelivery.net/',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-site',
+            "ping|activate": {
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "no-cache",
+                "origin": "https://iframe.mediadelivery.net",
+                "pragma": "no-cache",
+                "referer": "https://iframe.mediadelivery.net/",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site",
             },
-            'playlist': {
-                'authority': 'iframe.mediadelivery.net',
-                'accept': '*/*',
-                'accept-language': 'en-US,en;q=0.9',
-                'cache-control': 'no-cache',
-                'pragma': 'no-cache',
-                'referer': embed_url,
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-origin',
-            }
+            "playlist": {
+                "authority": "iframe.mediadelivery.net",
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "no-cache",
+                "pragma": "no-cache",
+                "referer": embed_url,
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+            },
         }
-        embed_response = self.session.get(embed_url,
-                                          headers=self.headers['embed'])
+        embed_response = self.session.get(embed_url, headers=self.headers["embed"])
         embed_page = embed_response.text
         try:
             self.server_id = re.search(
-                r'https://video-(.*?)\.mediadelivery\.net', embed_page).group(1)
+                r"https://video-(.*?)\.mediadelivery\.net", embed_page
+            ).group(1)
         except AttributeError:
             sys.exit(1)
-        self.headers['ping|activate'].update(
-            {'authority': f'video-{self.server_id}.mediadelivery.net'})
+        self.headers["ping|activate"].update(
+            {"authority": f"video-{self.server_id}.mediadelivery.net"}
+        )
         search = re.search(r'contextId=(.*?)&secret=(.*?)"', embed_page)
         self.context_id, self.secret = search.group(1), search.group(2)
         if name:
-            self.file_name = f'{name}.mp4'
+            self.file_name = f"{name}.mp4"
         else:
-            file_name_unescaped = re.search(r'og:title" content="(.*?)"',
-                                            embed_page).group(1)
+            file_name_unescaped = re.search(
+                r'og:title" content="(.*?)"', embed_page
+            ).group(1)
             file_name_escaped = unescape(file_name_unescaped)
-            self.file_name = re.sub(r'\.[^.]*$.*', '.mp4', file_name_escaped)
-            if not self.file_name.endswith('.mp4'):
-                self.file_name += '.mp4'
-        self.path = path if path else '~/Videos/Bunny CDN/'
+            self.file_name = re.sub(r"\.[^.]*$.*", ".mp4", file_name_escaped)
+            if not self.file_name.endswith(".mp4"):
+                self.file_name += ".mp4"
+        self.path = path if path else "~/Videos/Bunny CDN/"
 
     def prepare_dl(self) -> str:
-
-        def ping(time: int, paused: str, res: str):
+        def ping(time: float, paused: str, res: str):
             md5_hash = md5(
-                f'{self.secret}_{self.context_id}_{time}_{paused}_{res}'.encode(
-                    'utf8')).hexdigest()
+                f"{self.secret}_{self.context_id}_{time}_{paused}_{res}".encode("utf8")
+            ).hexdigest()
             params = {
-                'hash': md5_hash,
-                'time': time,
-                'paused': paused,
-                'chosen_res': res
+                "hash": md5_hash,
+                "time": time,
+                "paused": paused,
+                "chosen_res": res,
             }
             self.session.get(
-                f'https://video-{self.server_id}.mediadelivery.net/.drm/{self.context_id}/ping',
+                f"https://video-{self.server_id}.mediadelivery.net/.drm/{self.context_id}/ping",
                 params=params,
-                headers=self.headers['ping|activate'])
+                headers=self.headers["ping|activate"],
+            )
 
         def activate():
             self.session.get(
-                f'https://video-{self.server_id}.mediadelivery.net/.drm/{self.context_id}/activate',
-                headers=self.headers['ping|activate'])
+                f"https://video-{self.server_id}.mediadelivery.net/.drm/{self.context_id}/activate",
+                headers=self.headers["ping|activate"],
+            )
 
         def main_playlist():
-            params = {'contextId': self.context_id, 'secret': self.secret}
+            params = {"contextId": self.context_id, "secret": self.secret}
             response = self.session.get(
-                f'https://iframe.mediadelivery.net/{self.guid}/playlist.drm',
+                f"https://iframe.mediadelivery.net/{self.guid}/playlist.drm",
                 params=params,
-                headers=self.headers['playlist'])
-            resolutions = re.findall(r'\s*(.*?)\s*/video\.drm', response.text)[::-1]
+                headers=self.headers["playlist"],
+            )
+            resolutions = re.findall(r"\s*(.*?)\s*/video\.drm", response.text)[::-1]
             if not resolutions:
                 sys.exit(2)
             else:
                 return resolutions[0]  # highest resolution, -1 for lowest
 
         def video_playlist():
-            params = {'contextId': self.context_id}
+            params = {"contextId": self.context_id}
             self.session.get(
-                f'https://iframe.mediadelivery.net/{self.guid}/{resolution}/video.drm',
+                f"https://iframe.mediadelivery.net/{self.guid}/{resolution}/video.drm",
                 params=params,
-                headers=self.headers['playlist'])
+                headers=self.headers["playlist"],
+            )
 
-        ping(time=0, paused='true', res='0')
+        ping(time=0, paused="true", res="0")
         activate()
         resolution = main_playlist()
         video_playlist()
         for i in range(0, 29, 4):  # first 28 seconds, arbitrary (check issue#11)
-            ping(time=i + round(random(), 6),
-                 paused='false',
-                 res=resolution.split('x')[-1])
+            ping(
+                time=i + round(random(), 6),
+                paused="false",
+                res=resolution.split("x")[-1],
+            )
         self.session.close()
         return resolution
 
     def download(self):
         resolution = self.prepare_dl()
         url = [
-            f'https://iframe.mediadelivery.net/{self.guid}/{resolution}/video.drm?contextId={self.context_id}'
+            f"https://iframe.mediadelivery.net/{self.guid}/{resolution}/video.drm?contextId={self.context_id}"
         ]
         ydl_opts = {
-            'http_headers': {
-                'Referer': self.embed_url,
-                'User-Agent': self.user_agent['user-agent']
+            "http_headers": {
+                "Referer": self.embed_url,
+                "User-Agent": self.user_agent["user-agent"],
             },
-            'concurrent_fragment_downloads': 10,
+            "concurrent_fragment_downloads": 10,
             # 'external_downloader': 'aria2c'
-            'nocheckcertificate': True,
-            'outtmpl': self.file_name,
-            'restrictfilenames': True,
-            'windowsfilenames': True,
-            'nopart': True,
-            'paths': {
-                'home': self.path,
-                'temp': f'.{self.file_name}/',
+            "nocheckcertificate": True,
+            "outtmpl": self.file_name,
+            "restrictfilenames": True,
+            "windowsfilenames": True,
+            "nopart": True,
+            "paths": {
+                "home": self.path,
+                "temp": f".{self.file_name}/",
             },
-            'retries': float('inf'),
-            'extractor_retries': float('inf'),
-            'fragment_retries': float('inf'),
-            'skip_unavailable_fragments': False,
-            'no_warnings': True,
+            "retries": float("inf"),
+            "extractor_retries": float("inf"),
+            "fragment_retries": float("inf"),
+            "skip_unavailable_fragments": False,
+            "no_warnings": True,
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download(url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     video = BunnyVideoDRM(
         # insert the referer between the quotes below (address of your webpage)
-        referer='',
+        referer="",
         # paste your embed link
-        embed_url='',
+        embed_url="",
         # you can override file name, no extension
         name="",
         # you can override download path
-        path=r"")
+        path=r"",
+    )
     # video.session.close()
     video.download()
